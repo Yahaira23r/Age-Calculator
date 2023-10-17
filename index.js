81a320ee0cba7393401d6403c853712ee34641c3
Yahaira23r <145211412+Yahaira23r@users.noreply.github.com>
2023-10-16 19:52:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ function calculateAge() {
   }
 }
 
+
 function getAge(birthdayValue) {
   const currentDate = new Date();
   const birthdayDate = new Date(birthdayValue);
   let age = currentDate.getFullYear() - birthdayDate.getFullYear();
   const month = currentDate.getMonth() - birthdayDate.getMonth();
 
+  
   if (
     month < 0 ||
     (month === 0 && currentDate.getDate() < birthdayDate.getDate())
@@ -29,3 +31,4 @@ function getAge(birthdayValue) {
 }
 
 btnEl.addEventListener("click", calculateAge);
+
